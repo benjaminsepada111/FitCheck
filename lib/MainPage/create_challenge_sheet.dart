48b8c61da40136da114941d5eee61614b617fc09
Miao <egmiao24@gmail.com>
2025-12-02 23:39:12 +0800

    permissions not showing fix

--- lib/MainPage/create_challenge_sheet.dart
+++ lib/MainPage/create_challenge_sheet.dart
@@ -5,7 +5,6 @@ import 'package:capstone_project/services/user_data_service.dart';
 import 'package:capstone_project/services/calorie_calculator.dart';
 import 'package:capstone_project/services/challenge_service.dart';
 import 'package:capstone_project/models/user_data.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 class CreateChallengeSheet extends StatefulWidget {
   final dynamic Function(Challenge) onChallengeCreated;
@@ -130,17 +129,6 @@ class _CreateChallengeSheetState extends State<CreateChallengeSheet> {
     setState(() {});
   }
 
-  Future<bool> _requestCalendarPermission() async {
-    var status = await Permission.calendar.status;
-
-    if (status.isDenied || status.isRestricted) {
-      status = await Permission.calendar.request();
-    }
-
-    return status.isGranted;
-  }
-
-
   @override
   void dispose() {
     _pageController.dispose();
@@ -157,12 +145,6 @@ class _CreateChallengeSheetState extends State<CreateChallengeSheet> {
       TextEditingController controller,
       bool isStartDate,
       ) async {
-
-    bool granted = await _requestCalendarPermission();
-    if (!granted) {
-      _showError("Calendar permission is required.");
-      return;
-    }
 
     final DateTime? picked = await showDatePicker(
       context: context,

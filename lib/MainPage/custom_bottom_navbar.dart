84bff43cff19cf09890fbdd262415164410bb366
Miao <egmiao24@gmail.com>
2025-12-05 01:17:29 +0800

    navbar ios changes

--- lib/MainPage/custom_bottom_navbar.dart
+++ lib/MainPage/custom_bottom_navbar.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:capstone_project/color/colors.dart';
@@ -16,6 +17,7 @@ class CustomBottomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final r = context.responsive;
+    final isIOS = Platform.isIOS;
     return Container(
       decoration: BoxDecoration(
         color: Colors.white,
@@ -28,11 +30,13 @@ class CustomBottomNavBar extends StatelessWidget {
         ],
       ),
       child: SafeArea(
+        top: false,
         child: Container(
-          height: r.size(60),
+          height: isIOS ? r.size(55) : r.size(60),
           padding: r.paddingSymmetric(horizontal: 16),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               _buildNavItem(context, 0, "assets/icons/home.svg"),
               _buildNavItem(context, 1, "assets/icons/food.svg"),
@@ -47,6 +51,7 @@ class CustomBottomNavBar extends StatelessWidget {
 
   Widget _buildNavItem(BuildContext context, int index, String iconPath) {
     final r = context.responsive;
+    final isIOS = Platform.isIOS;
     final isSelected = currentIndex == index;
     return GestureDetector(
       onTap: () => onTap(index),
@@ -55,28 +60,25 @@ class CustomBottomNavBar extends StatelessWidget {
           minWidth: r.tapTarget(44),
           minHeight: r.tapTarget(44),
         ),
-        padding: r.paddingSymmetric(horizontal: 16, vertical: 8),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              padding: r.padding(all: 6),
-              decoration: BoxDecoration(
-                color: isSelected ? AppColors.secondary.withValues(alpha: 0.1) : Colors.transparent,
-                borderRadius: BorderRadius.circular(r.size(10)),
-              ),
-              child: SvgPicture.asset(
-                iconPath,
-                colorFilter: ColorFilter.mode(
-                  isSelected ? AppColors.secondary : Colors.grey.shade600,
-                  BlendMode.srcIn,
-                ),
-                height: r.size(28),
-                width: r.size(28),
-              ),
+        padding: isIOS 
+          ? r.paddingSymmetric(horizontal: 16, vertical: 0)
+          : r.paddingSymmetric(horizontal: 16, vertical: 8),
+        alignment: Alignment.center,
+        child: Container(
+          padding: isIOS ? r.padding(all: 6) : r.padding(all: 6),
+          decoration: BoxDecoration(
+            color: isSelected ? AppColors.secondary.withValues(alpha: 0.1) : Colors.transparent,
+            borderRadius: BorderRadius.circular(r.size(10)),
+          ),
+          child: SvgPicture.asset(
+            iconPath,
+            colorFilter: ColorFilter.mode(
+              isSelected ? AppColors.secondary : Colors.grey.shade600,
+              BlendMode.srcIn,
             ),
-          ],
+            height: isIOS ? r.size(30) : r.size(28),
+            width: isIOS ? r.size(30) : r.size(28),
+          ),
         ),
       ),
     );
